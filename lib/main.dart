d3fb704c51c1fdada7d9a8e4a53576ef9cd84161
Vito Maiocchi <vito.maiocchi@outlook.com>
2026-05-08 16:32:16 +0200

    drag: maxdrag + haptic feedback

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:music_client/music_providers.dart';
+import 'package:flutter/services.dart';
+import 'package:just_audio/just_audio.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -31,13 +35,15 @@ class SwipeableTile extends StatefulWidget {
   final VoidCallback onSwipeLeft;
   final VoidCallback onSwipeRight;
   final double threshold;
+  final double maxDrag;
 
   const SwipeableTile({
     super.key,
     required this.child,
     required this.onSwipeLeft,
     required this.onSwipeRight,
-    this.threshold = 100,
+    this.threshold = 40,
+    this.maxDrag = 80,
   });
 
   @override
@@ -47,26 +53,54 @@ class SwipeableTile extends StatefulWidget {
 class _SwipeableTileState extends State<SwipeableTile>
     with SingleTickerProviderStateMixin {
   late AnimationController _controller;
-  late Animation<double> _animation;
+  late AudioPlayer _audioPlayer;
   double _dragOffset = 0;
+  bool _overThreshold = false;
+
+  static bool get _isDesktop =>
+      Platform.isLinux || Platform.isWindows || Platform.isMacOS;
 
   @override
   void initState() {
     super.initState();
     _controller = AnimationController(
       vsync: this,
-      duration: const Duration(milliseconds: 300),
+      duration: const Duration(milliseconds: 200),
     );
+    if (_isDesktop) {
+      _audioPlayer = AudioPlayer();
+      // drop a short click wav in assets/sounds/click.wav
+      _audioPlayer.setAsset('assets/sounds/click.wav');
+    }
   }
 
   @override
   void dispose() {
     _controller.dispose();
+    if (_isDesktop) _audioPlayer.dispose();
     super.dispose();
   }
 
+  Future<void> _triggerFeedback() async {
+    if (_isDesktop) {
+      await _audioPlayer.seek(Duration.zero);
+      await _audioPlayer.play();
+    } else {
+      await HapticFeedback.lightImpact();
+    }
+  }
+
   void _onDragUpdate(DragUpdateDetails details) {
-    setState(() => _dragOffset += details.delta.dx);
+    setState(() {
+      _dragOffset += details.delta.dx;
+      _dragOffset = _dragOffset.clamp(-widget.maxDrag, widget.maxDrag);
+    });
+
+    final isOver = _dragOffset.abs() >= widget.threshold;
+    if (isOver != _overThreshold) {
+      _overThreshold = isOver;
+      _triggerFeedback();
+    }
   }
 
   void _onDragEnd(DragEndDetails details) {
@@ -78,23 +112,43 @@ class _SwipeableTileState extends State<SwipeableTile>
       widget.onSwipeRight();
     }
 
-    // snap back
-    _animation = Tween<double>(begin: _dragOffset, end: 0).animate(
-      CurvedAnimation(parent: _controller, curve: Curves.elasticOut),
-    )..addListener(() => setState(() => _dragOffset = _animation.value));
+    _overThreshold = false;
+    final start = _dragOffset;
+    _controller.reset();
 
-    _controller.forward(from: 0);
+    final animation = Tween<double>(
+      begin: start,
+      end: 0,
+    ).animate(CurvedAnimation(parent: _controller, curve: Curves.easeOut));
+    animation.addListener(() => setState(() => _dragOffset = animation.value));
+
+    _controller.forward();
   }
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onHorizontalDragUpdate: _onDragUpdate,
-      onHorizontalDragEnd: _onDragEnd,
-      child: Transform.translate(
-        offset: Offset(_dragOffset, 0),
-        child: widget.child,
-      ),
+    return Stack(
+      children: [
+        Positioned.fill(
+          child: Row(
+            children: [
+              if (_dragOffset > 0)
+                Container(width: _dragOffset, color: Colors.orange),
+              const Spacer(),
+              if (_dragOffset < 0)
+                Container(width: -_dragOffset, color: Colors.orange),
+            ],
+          ),
+        ),
+        Transform.translate(
+          offset: Offset(_dragOffset, 0),
+          child: GestureDetector(
+            onHorizontalDragUpdate: _onDragUpdate,
+            onHorizontalDragEnd: _onDragEnd,
+            child: widget.child,
+          ),
+        ),
+      ],
     );
   }
 }
